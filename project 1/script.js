12938f79354a1b57e885c9f83c0051e4da80c78c
hackshot12Mr <105168986+hackshot12Mr@users.noreply.github.com>
2022-07-18 16:14:24 +0100

    Add files via upload

---
+++ project 1/script.js
@@ -0,0 +1,45 @@
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains3 = document.getElementById('mountains3');
+let mountains4 = document.getElementById('mountains4');
+let river = document.getElementById('river');
+let boat = document.getElementById('boat');
+let mountains7 = document.getElementById('mountains7');
+let nouvil = document.querySelector('.nouvil');
+
+
+window.onscroll = function(){
+    let value = scrollY
+    stars.style.left = value + 'px';
+    moon.style.top = value * 3 + 'px';
+    mountains3.style.top = value * 2 + 'px';
+    mountains4.style.top = value * 1.5 + 'px';
+    river.style.top = value * 3 + 'px';
+    boat.style.top = value  + 'px';
+    boat.style.left = value * 3 + 'px';
+    nouvil.style.fontSize = value + 'px';
+    if(scrollY >= 67){
+      nouvil.style.fontSize = 67 + 'px';   
+      nouvil.style.position = 'fixed';
+      if(scrollY >=478){
+        nouvil.style.display = 'none';
+      }else{
+        nouvil.style.display = 'block';
+      }
+      if(scrollY >= 127){
+        document.querySelector('.main').style.background = 'linear-gradient(#231476,#a985cb)';
+      }else{
+        document.querySelector('.main').style.background = 'linear-gradient(#030303,#10001f)';
+      }
+
+    }
+   
+
+   
+  
+}
+
+
+
+
+
